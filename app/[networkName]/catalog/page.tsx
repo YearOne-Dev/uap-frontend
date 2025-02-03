6e7d82ffb38794272d0a0f6a68a0c82e6329139c
democrazyfi <132473036+democrazyfi@users.noreply.github.com>
2025-02-03 16:54:04 +0400

    remove hardcode

--- app/[networkName]/catalog/page.tsx
+++ app/[networkName]/catalog/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import AssistantInfo from '@/components/AssistantInfo';
-import { networkNameToIdMapping, supportedNetworks } from "@/constants/supportedNetworks";
+import { burntPixRefinerTestnet } from "@/constants/assistantsConfig";
 
 export default function CatalogPage({
   params,
@@ -36,42 +36,6 @@ export default function CatalogPage({
               fontWeight={700}
               mb={4}
             >
-              Executive Assistants
-            </Box>
-            <Box
-              border="1px solid"
-              borderColor="uap.font"
-              borderRadius={10}
-              p={4}
-            >
-              <AssistantInfo
-                assistant={{
-                  address: '0x7870C5B8BC9572A8001C3f96f7ff59961B23500D',
-                  name: 'Universal Profile Assistant',
-                  description: 'The Universal Profile Assistant is a decentralized application that allows users to manage their universal profiles.',
-                  iconPath: '/lyx_icon_mainnet.svg',
-                  links: [{
-                    name: 'X',
-                    url: 'https://x.com/yearone_io',
-                  }],
-                  creatorAddress: '0x',
-                  supportedTransactionTypes: [],
-                  chainId: networkNameToIdMapping[params.networkName],
-                  assistantType: 'Executive',
-                  configParams: [],
-                }}
-                includeLink
-              />
-            </Box>
-          </Box>
-          <Box flex="1">
-            <Box
-              color="uap.font"
-              fontFamily="Montserrat"
-              fontSize={['lg', 'xl', '2xl']} // Responsive font size
-              fontWeight={700}
-              mb={4}
-            >
               Screener Assistants
             </Box>
             <Box
@@ -80,21 +44,7 @@ export default function CatalogPage({
               borderRadius={10}
               p={4}
             >
-              <AssistantInfo assistant={{
-                address: '0x7870C5B8BC9572A8001C3f96f7ff59961B23500D',
-                name: 'Universal Profile Assistant',
-                description: 'The Universal Profile Assistant is a decentralized application that allows users to manage their universal profiles.',
-                iconPath: '/lyx_icon_mainnet.svg',
-                links: [{
-                  name: 'X',
-                  url: 'https://x.com/yearone_io',
-                }],
-                creatorAddress: '0x',
-                supportedTransactionTypes: [],
-                chainId: networkNameToIdMapping[params.networkName],
-                assistantType: 'Screener',
-                configParams: [],
-              }} includeLink />
+              <AssistantInfo assistant={burntPixRefinerTestnet} includeLink />
             </Box>
           </Box>
         </Flex>
